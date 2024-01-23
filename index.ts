5034470e5a60839d209ad67e72a7337e7d5815f3
Tyrone Wong <tyrone@statsig.com>
2024-01-23 07:44:16 -0800

    10

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as gcp from "@pulumi/gcp";
 // Create a GCP resource (Storage Bucket)
 
 const name = [];
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 10; i++) {
   const bucket = new gcp.storage.Bucket(`tydev-my-bucket-${i}`, {
     location: "US",
     project: "statsig-services",
