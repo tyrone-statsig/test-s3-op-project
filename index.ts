69a9b4892dda9722cc84f028fff451e5ae5e329e
Tyrone Wong <tyrone@statsig.com>
2024-01-31 16:53:43 -0800

    make 2

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as pulumi from "@pulumi/pulumi";
 
 import { createBucket } from "./resources/gcp/storage/buckets";
 
-let buckets = createBucket("tydev-new-argocd-stck", 1);
+let buckets = createBucket("tydev-new-argocd-stck", 2);
 
 // Export the DNS name of the bucket
 export const bucketName = buckets;
